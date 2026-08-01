8bbd616a49071d10139f332660094120f63ba993
Yash <yashrp018@gmail.com>
2026-08-01 18:04:39 +0530

    Updated code

--- fetch_test.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-https.get('https://shivshaktiwaterequipment.com/', (res) => {
-  let data = '';
-  res.on('data', (chunk) => data += chunk);
-  res.on('end', () => {
-    const cssMatches = data.match(/href="([^"]+\.css)"/g);
-    console.log("CSS files found:", cssMatches);
-  });
-});
